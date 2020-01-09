4e860573a5fb50c08ed61ff7caaefc609850c346
Aaron Arsenault <arsena@gmail.com>
2020-01-09 14:40:47 -0500

    introducing abstraction of the BotClient, introducing the LexClient subclass

--- src/cucumberSupport/chatpickle.steps.js
+++ src/cucumberSupport/chatpickle.steps.js
@@ -1,6 +1,6 @@
 const { Before, Given, When, Then, setDefaultTimeout } = require('cucumber');
 const { assert } = require('chai');
-const { BotClient } = require('../lib/botClients/botClient.js');
+const { LexClient } = require('../lib/botClients/LexClient.js');
 const CHATPICKLE_CONFIG = require(`${process.env.CHATPICKLE_CONSUMER_PATH_ABSOLUTE}/chatpickle.config.json`);
   
 const CUCUMBER_STEPS_TIMEOUT_MILLISECONDS = 30000;
@@ -23,7 +23,11 @@ Given('the user is {string}', function (userName) {
 Given('the user begins a new chat with {string}', function (botName) {
     const botConfig = CHATPICKLE_CONFIG.bots[botName];
     assert.ok(botConfig, `Missing config for bot name ${botName} - add it to chatpickle.config.bots`);
-    this.botClient = new BotClient(botConfig.name, botConfig.alias, botConfig.region, this.userName, this.userAttributes);
+    const userConfig = {
+        userName: this.userName,
+        userAttributes: this.userAttributes
+    };
+    this.botClient = new LexClient(botConfig, userConfig);
 });
   
 When(/User:\s*([^\n\r]*)/i, async function (inputText) {
---
+++ src/lib/botClients/lexClient.ts
@@ -0,0 +1,47 @@
+import LexRuntime from 'aws-sdk/clients/lexruntime';
+import { BotClient } from './botClient';
+
+export class LexClient extends BotClient{
+
+    private botName: string;
+    private botAlias: string;
+    private userId: string;
+    private sessionAttributes: any;
+    private lex: LexRuntime;
+
+    constructor(botContext: any, userContext: any) {
+        super(botContext, userContext);
+        this.botName = this.botContext.botName;
+        this.botAlias = this.botContext.botAlias;
+        this.userId = `${this.userContext.userName}-${Date.now()}`;
+        this.sessionAttributes = this.userContext.userAttributes;
+        this.lex = new LexRuntime({region: this.botContext.region});
+        console.log(`[${this.userId}] New Conversation with ${this.botName}`);
+    }
+
+    public async speak(inputText: string): Promise<string> {
+        try {
+            const params = {
+                botName: this.botName,
+                botAlias: this.botAlias,
+                userId: this.userId,
+                inputText,
+                sessionAttributes: this.sessionAttributes
+            };
+
+            console.log(`[${this.userId}] User: ${inputText}`);
+
+            const response = await this.lex.postText(params).promise();
+
+            const reply: string = response.message.trim();
+            this.sessionAttributes = response.sessionAttributes;
+
+            console.log(`[${this.userId}] Bot: ${reply}`);
+
+            return reply;
+        } catch (e) {
+            throw e;
+        }
+    }
+
+}
