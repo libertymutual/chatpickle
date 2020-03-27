d5f21d7ff3d7c79dc770133faf88d123b6e5172f
Aaron Arsenault <arsena@gmail.com>
2020-03-27 09:30:32 -0400

    Cleanup

--- src/lib/botClients/lexClient.test.ts
+++ src/lib/botClients/lexClient.test.ts
@@ -1,11 +1,3 @@
-console.log('Sorted ENV');
-console.log(
-    Object.keys(process.env)
-        .map(key => `${key}=${process.env[key]}`)
-        .sort()
-        .join('\n'),
-);
-
 // Need to bypass type safety of typescript to allow this approach for mocking to work.
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const LexRuntime = require('aws-sdk/clients/lexruntime');
