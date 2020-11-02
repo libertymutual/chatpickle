def1222a4364d975ed520639c0aadbf9edd4e1ef
Aaron Arsenault <arsena@gmail.com>
2020-11-02 13:05:30 -0500

    Added botConfig.type === 'custom' so that users can define their own in-house bot client interfaces

--- src/cucumberSupport/chatpickle.steps.js
+++ src/cucumberSupport/chatpickle.steps.js
@@ -17,7 +17,6 @@ Given('the user is {string}', function(userName) {
     assert.ok(CHATPICKLE_CONFIG.users, `Missing chatpickle.config.json attribute users`);
     const userConfig = CHATPICKLE_CONFIG.users[userName];
 
-    // TODO - implement config validations that are elegant
     assert.ok(userConfig, `Missing config for users.${userName}`);
     assert.ok(userConfig.context, `Missing config for users.${userName}.context`);
     assert.ok(userConfig.context.userId, `Missing config for users.${userName}.context.userId`);
@@ -30,12 +29,17 @@ Given('the user begins a new chat with {string}', function(botName) {
     assert.ok(CHATPICKLE_CONFIG.bots, `Missing chatpickle.config.json attribute bots`);
     const botConfig = CHATPICKLE_CONFIG.bots[botName];
 
-    // TODO - implement config validations that are elegant
     assert.ok(botConfig, `Missing config for bots.${botName}`);
     assert.ok(botConfig.type, `Missing config for bots.${botName}.type`);
     assert.ok(botConfig.context, `Missing config for bots.${botName}.context`);
 
-    const BotSubclass = require(`../lib/botClients/${botConfig.type}Client.js`).default;
+    let BotSubclass;
+    if (botConfig.type === 'custom') {
+        assert.ok(botConfig.location, `Missing config for bots.${botName}.location`);
+        BotSubclass = require(`${process.env.CHATPICKLE_CONSUMER_PATH_ABSOLUTE}/${botConfig.location}`).default;
+    } else {
+        BotSubclass = require(`../lib/botClients/${botConfig.type}Client`).default;
+    }
     this.botClient = new BotSubclass(botConfig.context, this.userContext);
 });
 
